b6089dcb8015a9d4c13cceece5d0e380fcb5f9c4
imikh1991 <ilya.mikhaylyukov@gmail.com>
2023-08-26 20:41:36 +0700

    add: form-validator-yup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import bootstrap from 'bootstrap';
-
 // @ts-check
-
 // eslint-disable-next-line no-console
-console.error(new Error('Whoops, something bad happened'));
+import app from './validate.js';
+
+app();
---
+++ src/validate.js
@@ -0,0 +1,46 @@
+import * as yup from 'yup';
+import onChange from 'on-change';
+
+export default () => {
+  const initialState = {
+    url: [],
+    isValid: true,
+    error: '',
+  };
+
+  const form = document.querySelector('.rss-form');
+  const inputField = document.querySelector('#url-input');
+  const feedbackElement = document.querySelector('.feedback');
+
+  const watchedState = onChange(initialState, (path, value) => {
+    if (watchedState.isValid) {
+      feedbackElement.textContent = 'RSS успешно загружен';
+      feedbackElement.classList.replace('text-danger', 'text-success');
+      inputField.classList.remove('is-invalid');
+      inputField.focus();
+      inputField.value = '';
+    } else {
+      feedbackElement.textContent = watchedState.error;
+      feedbackElement.classList.replace('text-success', 'text-danger');
+      inputField.classList.add('is-invalid');
+    }
+  });
+
+  const schema = yup.string().required()
+    .url('Ссылка должна быть валидным URL')
+    .notOneOf(watchedState.url, 'RSS уже существует');
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const url = formData.get('url');
+    try {
+      await schema.validate(url);
+      watchedState.isValid = true;
+      watchedState.url.push(url);
+    } catch (error) {
+      watchedState.isValid = false;
+      watchedState.error = error.message;
+    }
+  });
+};
