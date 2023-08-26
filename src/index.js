4bbaa1b7f137a07329fa5e8303b69012acb7c803
imikh1991 <ilya.mikhaylyukov@gmail.com>
2023-08-26 19:59:11 +0700

    add: basic-structure-deploy-ver.1.4

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@
 
 // @ts-check
 
-// eslint-disable-next-line import/extensions
-import init from './init.js';
-import './style.css';
-
-init();
+// eslint-disable-next-line no-console
+console.error(new Error('Whoops, something bad happened'));
